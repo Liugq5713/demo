1ddf221d5ce9318b2ed5bd7f6c570b7fdc6178b6
刘光启 <liuguangqi@bytedance.com>
2019-11-19 17:02:12 +0800

    i am lazy

--- demo-js/async.js
+++ demo-js/async.js
@@ -1,13 +1,15 @@
 function getSomeAfterOneSecond() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve('done')
-    }, 1000)
-  })
+      console.log("run,11");
+
+      resolve("done");
+    }, 1000);
+  });
 }
 
 function isDef(v) {
-  return v !== undefined && v !== null
+  return v !== undefined && v !== null;
 }
 
 function isPromise(val) {
@@ -15,14 +17,29 @@ function isPromise(val) {
     isDef(val) &&
     typeof val.then === "function" &&
     typeof val.catch === "function"
-  )
+  );
 }
 
 async function main() {
-  const a = await getSomeAfterOneSecond()
-  console.log(a)
-  const b = getSomeAfterOneSecond()
-  console.log({ b }, isPromise(b))
+  const a = await getSomeAfterOneSecond();
+  console.log(a);
+  const b = getSomeAfterOneSecond();
+  console.log({ b }, isPromise(b));
 }
 
-main()
+const myRequest = async func => {
+  const res = await func();
+  return res;
+};
+
+const add = () => {
+  console.log("run");
+  return "this is no result";
+};
+async function test() {
+  const res = await myRequest(add);
+  const res2 = await myRequest(getSomeAfterOneSecond);
+  console.log({ res, res2 });
+}
+test();
+// main();
